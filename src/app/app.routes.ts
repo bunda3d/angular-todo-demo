0535188329d792847e1a38287240f596efa83eb6
bunda3d <kris@krisbunda.com>
2026-09-13 00:02:54 -0500

    add counter component, todos page, move home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent }
+  { 
+    path: '', 
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./home/home').then((m) => m.Home);
+    },
+  },
 ];
---
+++ src/app/home/home.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { Greeting } from './components/greeting/greeting';
+import { Counter } from './components/counter/counter';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [Greeting, Counter],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  homeMessage = signal('Welcome to your TO-DOs app');
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key. `)
+  }
+}
