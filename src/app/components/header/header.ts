0a486c382c00b9bf689eea47884d3ff889eef4b6
bunda3d <kris@krisbunda.com>
2026-09-12 19:58:39 -0500

    add greeting component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
-
+  title = signal('ToDo-App');
 }
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Greeting } from '../greeting/greeting';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Greeting],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+  homeMessage = signal('Welcome to your TO-DOs app');
 }
